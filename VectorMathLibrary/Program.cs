63eea434abf32687fbc16d2fddc1a438ce8c5b24
Legus <sjlegus@seattleschools.org>
2022-02-14 14:11:33 -0800

    First

---
+++ VectorMathLibrary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VectorMathLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2 u = new Vector2(1, 1);
+            Vector2 v = new Vector2(2, 3);
+            Vector2 r = new Vector2(-1, 2);
+
+            Console.WriteLine(5f * v);
+            Console.WriteLine(v * 5f);
+        }
+    }
+}
---
+++ VectorMathLibrary/Vector2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorMathLibrary
+{
+    public class Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float magnitude
+        {
+            get
+            {
+                return MathF.Sqrt((x * x) + (y * y));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            Vector2 ans = new Vector2(a.x + b.x, a.y + b.y);
+            return ans;
+        }
+
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            Vector2 ans = new Vector2(a.x - b.x, a.y - b.y);
+            return ans;
+        }
+
+        public static Vector2 operator *(float a, Vector2 b)
+        {
+            return new Vector2(a * b.x, a * b.y);
+        }
+
+        public static Vector2 operator *(Vector2 b, float a)
+        {
+            return new Vector2(a * b.x, a * b.y);
+        }
+    }
+}
